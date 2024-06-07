a9e45a927f51ca00e2767b6df346a39f5ad5d8ee
ISAAC Marc <marc.isaac@colombbus.org>
2024-06-07 16:19:51 +0200

    base de donné massy

--- 01_SQL/db.sql
+++ 01_SQL/db.sql
@@ -1,4 +1,11 @@
-
+SELECT * FROM abonnes WHERE id_abonne IN(SELECT id_abonnes FROM emprunt WHERE id_livres IN (SELECT id_livre FROM livres WHERE auteur LIKE '%DAUDET%'));
+SELECT titre, id_livre FROM livres WHERE id_livre IN (SELECT id_livre);
+SELECT titre, id_livre FROM livres WHERE id_livre IN (SELECT id_livres FROM emprunt WHERE id_abonnes IN(SELECT id_abonne FROM abonnes WHERE prenom = 'Benoit'));
+SELECT titre, id_livre FROM livres WHERE id_livre NOT IN (SELECT id_livres FROM emprunt WHERE id_abonnes IN(SELECT id_abonne FROM abonnes WHERE prenom = 'Benoit'));
+SELECT titre FROM livres WHERE id_livre IN(SELECT id_livre FROM emprunt WHERE date_rendu IS NULL AND id_abonnes IN(SELECT id_abonne FROM abonnes WHERE prenom = 'Benoit'));
+SELECT COUNT (*) FROM livres WHERE id_livre IN(SELECT id_livre FROM emprunt WHERE id_abonnes IN(SELECT id_abonne FROM abonnes WHERE prenom = 'Guillaume'));
+SELECT COUNT(id_livre) AS  nombreEmpruntGuillaume FROM emprunt WHERE id_abonne IN(select id_abonne);
+SELECT titre FROM livres WHERE id_livre NOT IN (SELECT id_livres FROM emprunt);
 
 
 
